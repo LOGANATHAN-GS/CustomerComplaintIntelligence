c5712668f063efe0ccfa6477ec09c57def78aa3c
LOGANATHAN-GS <gsloganathan2007@gmail.com>
2026-08-24 23:52:42 +0530

    Initial project setup

---
+++ src/main/java/com/customercomplaint/CustomerComplaintApplication.java
@@ -0,0 +1,12 @@
+package com.customercomplaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerComplaintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerComplaintApplication.class, args);
+    }
+}
